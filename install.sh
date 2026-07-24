043b2455a898f36133b6d34461619baa321eea20
rafmarsan <rafmarsan.oficial@gmail.com>
2026-07-24 11:09:38 +0200

    Add mock install file

---
+++ install.sh
@@ -0,0 +1,7 @@
+brew install neovim git ripgrep fd
+brew install lua-language-server
+brew install basedpyright
+brew install gopls
+npm install -g @ansible/ansible-language-server
+
+uv tool install ansible-core==2.16.14
