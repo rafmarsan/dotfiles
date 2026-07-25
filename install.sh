9774bab594bb5bcd5c8c3fb45a9a590e0ad1605f
rafmarsan <rafmarsan.oficial@gmail.com>
2026-07-25 22:27:57 +0200

    fix(brew) : Add homebrew to PATH

--- install.sh
+++ install.sh
@@ -6,6 +6,12 @@ if ! command -v brew >/dev/null 2>&1; then
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
+if [[ -x /home/linuxbrew/.linuxbrew/bin/brew ]]; then
+    eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+elif [[ -x /opt/homebrew/bin/brew ]]; then
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+fi
+
 case "$(uname -s)" in
     Darwin)
         ./install/macos.sh
